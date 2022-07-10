008891598ce4146373c9169dff62d380eea35220
cyber-kid <102310990+CYBERXKID@users.noreply.github.com>
2022-07-10 20:56:53 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //════════════(00)════════════════\\
 //
-//          RED DRAGON v6.0
+//          ALEXA QUEEN v6.0
 //
 //════════════(00)════════════════\\
 
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `MY NAME IS ALEXA QUEEN🧚
+MY CREATOR IS CYBERXKID👩‍💻` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+27686881509'] //Owner number in aive msg
+global.premium = ['+27686881509'] //Owner Number info
+global.ownernomer = '+27686881509' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '👩‍💻𝙲𝚈𝙱𝙴𝚁𝚇𝙺𝙸𝙳🧚‍♂️' //Owner Name
+global.botname = ' 🧚‍♂️𝙰𝙻𝙴𝚇𝙰 𝚀𝚄𝙴𝙴𝙽🧚‍♂️' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
+global.button = '📱Youtube📱' //Costomize A Button Name In Alive Message
 global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
-global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
+global.packname = '🧚‍♂️𝙰𝙻𝙴𝚇𝙰 𝚀𝚄𝙴𝙴𝙽🧚‍♂️' //Bot Pacage Name
+global.author = '👩‍💻𝙲𝚈𝙱𝙴𝚁𝚇𝙺𝙸𝙳👩‍💻' //Author Name
+global.prefa = ['','!','.','👩‍💻','❤️','🧚‍♂️'] //Command Prefix eg:(.alive ,alive 👩‍💻alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_Cyberxkid.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/CYBERXKID' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
